2b7e46423f371124af1ad69ee5e6135cb5e0ff36
ghostclog <kdy11281@gmail.com>
2023-11-14 19:11:12 +0900

    커밋 푸쉬. 그리고, 풀에서 충돌발생해서, 해당 부분 해결
    유저app의 기본 골자 구성 완료

--- userApp/views.py
+++ userApp/views.py
@@ -12,7 +12,7 @@ def regist(request):
 
         user = UserData(user_id=user_id, user_pass=user_pass, user_email=user_email, user_tier = user_tier)  # UserData 객체 생성
         user.save()  # 데이터베이스에 저장 / 해싱도 같이 해줌
-        return render(request, 'home.html')
+        return HttpResponse('회원가입 완료')
     
 #로그인
 def login(request):
@@ -27,3 +27,55 @@ def login(request):
                 return HttpResponse('비번이 틀림')
         except:
             return HttpResponse('해당하는 아이디가 없음')
+        
+#이메일 보내기    
+def email_send(request):
+    if request.method == 'POST':
+        pass
+    #프론트에서 인증하고자하는 이메일을 보낼 경우,
+    #해당 이메일에 인증 번호를 발송함.
+    #그리고, 해당 이메일과 인증 번호를 데이터베이스에 저장
+
+#이메일 인증
+def email_chk(request):
+    if request.method == 'POST':
+        pass
+    #프론트에서 이메일과 인증 번호 발송시
+    #데이터베이스에 저장된 이메일과 인증번호 값을 체크
+    #존재한다면 true/아니면 false
+
+#아이디 중복 확인
+def id_chk(request):
+    if request.method == 'POST':
+        pass
+    #만약 프론트에서 쏴준 아이디가 데이터베이스에 존재하는지 확인
+
+#아이디 찾기
+def find_id(request):
+    if request.method == 'POST':
+        pass
+    #?? 어캐 할지 아직 모름
+
+#비밀번호 수정
+def ch_password(request):
+    if request.method == 'POST':
+        pass
+    #프론트에서 아이디와 비번을 주면, 해당 아이디에 맞는 비번 컬럼값을 수정
+
+#비밀번호 수정 전, 비밀번호 인증
+def rechk_password(request):
+    if request.method == 'POST':
+        pass
+    #비밀번호 체크를 사용.
+
+#사용자 카테고리 수정
+def edit_category(request):
+    if request.method == 'POST':
+        pass
+    #???
+
+#마이페이지 정보
+def mypage_info(request):
+    if request.method == 'POST':
+        pass
+    #(티어, 카테고리, 작성, 찜한 글, 찜한 강의) 반환
