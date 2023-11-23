30f9eb3f3604c7ee3552e4d1caff844e1e8169e9
ghostclog <kdy11281@gmail.com>
2023-11-23 19:17:57 +0900

    강의 정보 보기 view 제작

--- mainPostApp/views.py
+++ mainPostApp/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse,JsonResponse
+from django.db.models import Avg
 
-
-
-from .models import LectureInfo, LectureCategories, LectureReview
+from .models import TotalCategories, LectureInfo, LectureCategories, LectureReview
 
 
 
@@ -32,10 +32,43 @@ def write_lecture(request):
     #(티어, 카테고리, 작성, 찜한 글, 찜한 강의) 반환
 
 #강의 정보 보기
+from django.core import serializers
+
 def lecture_info(request):
     if request.method == 'POST':
-        pass
-    #(티어, 카테고리, 작성, 찜한 글, 찜한 강의) 반환
+        lecture_id = request.POST['lecture_id']
+        try:
+            # 강의 정보
+            lecture_info = LectureInfo.objects.get(lecture_id= lecture_id)
+            
+            # 강의 카테고리 정보
+            categories_info = LectureCategories.objects.filter(lecture_id = lecture_id).values_list('category_id', flat=True)
+            lecture_category = []
+            for i in categories_info:
+                lecture_category.append(TotalCategories.objects.filter(category_id = i).values('category_name'))
+            
+            # 강의 리뷰 및 댓글
+            lecture_review = LectureReview.objects.filter(lecture_id= lecture_id)
+            lecture_review = serializers.serialize('json', lecture_review)
+            
+            # 강의 별점
+            lecture_star = LectureReview.objects.filter(lecture_id= lecture_id).aggregate(Avg('review_star'))['review_star__avg']
+
+            # 반환 데이터
+            lecture_info_data = {
+                'lecture_id' : lecture_info.lecture_id,
+                'lecture_name' : lecture_info.lecture_name,
+                'appropriate_grade' : lecture_info.appropriate_grade,
+                'platform' : lecture_info.platform,
+                'lecture_infomation' : lecture_info.lecture_infomation,
+                'lecture_category':lecture_category,
+                'lecture_star' : lecture_star
+            }
+            return JsonResponse({'lecture_info_data':lecture_info_data,'lecture_review':lecture_review})
+        except:
+            return JsonResponse({'message':"잘못된 강의입니다."})
+
+
 
 #강의에 리뷰 작성하기
 def write_review(request):
